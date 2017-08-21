39ce8d36a8cbeb313e297d5ab6e8e469b82a0b9c
shuimo1 <guan1502351@163.com>
2017-08-21 19:50:20 +0800

    添加git()

--- GitFirst/src/com/test/git/TestGit.java
+++ GitFirst/src/com/test/git/TestGit.java
@@ -2,5 +2,11 @@ package com.test.git;
 
 public class TestGit {
 	int x=80;
-
+	public static void main(String[] args) {
+		git();
+	}
+	
+	public static void git(){
+		System.out.println("git+me+first");
+	}
 }
